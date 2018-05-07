02b62115a1d254ed30fe7c62525d61e5378cfd02
Antti Kivi <anttikiviyo@gmail.com>
2018-05-07 08:52:42 +0300

    Copy file, not a tree

--- utils/products/stb/build.py
+++ utils/products/stb/build.py
@@ -12,33 +12,12 @@
 
 
 import os
-import platform
 
 from build_utils import shell, workspace
 
 from products import common
 
 from script_support import data
-
-from script_support.variables import ANTHEM_REPO_NAME, ANTHEM_SOURCE_ROOT
-
-
-def _create_cxx_header():
-    cxx_header = os.path.join(data.build.local_root, "include", "lua.hpp")
-    if not os.path.exists(cxx_header):
-        with open(cxx_header, "w+") as outfile:
-            outfile.write("// lua.hpp\n")
-            outfile.write("// Lua header files for C++\n")
-            outfile.write(
-                "// <<extern \"C\">> not supplied automatically because Lua "
-                "also compiles as C++\n")
-            outfile.write("\n")
-            outfile.write("extern \"C\" {\n")
-            outfile.write("#include \"lua.h\"\n")
-            outfile.write("#include \"lualib.h\"\n")
-            outfile.write("#include \"lauxlib.h\"\n")
-            outfile.write("}\n")
-            outfile.write("")
 
 
 def _build():
@@ -52,7 +31,7 @@ def _build():
     if not workspace.is_include_dir_made() and workspace.include_file_exists(
             path="stb_image.h"):
         shell.rm(include)
-    shell.copytree(os.path.join(source_dir, "stb_image.h"), include)
+    shell.copy(os.path.join(source_dir, "stb_image.h"), include)
 
 
 def do_build():
