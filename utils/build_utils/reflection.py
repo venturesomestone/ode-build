18afe7e53e0489b86f4280739db824c25379113a
Antti Kivi <anttikiviyo@gmail.com>
2018-07-07 23:24:31 +0300

    :art: Move the common build parts of Unsung Anthem and Obliging Ode to the Unsung Anthem repository

--- utils/build_utils/reflection.py
+++ utils/build_utils/reflection.py
@@ -21,23 +21,31 @@ from script_support.variables import ANTHEM_SOURCE_ROOT, ANTHEM_REPO_NAME
 
 from . import diagnostics
 
+from .mapping import Mapping
+
 
 PRODUCT_PACKAGE = "products"
 CHECKOUT_MODULE = "checkout"
-BUILD_MODULE = "build"
+BUILD_MODULE = "build.py"
+CMAKE_MODULE = "cmake.py"
+DIRECTORY_MODULE = "directory.py"
+
+COMMON_PRODUCT = Mapping(key="common")
+
+PRODUCT_PATH = os.path.join(
+    ANTHEM_SOURCE_ROOT, ANTHEM_REPO_NAME, "utils", "products")
 
 
 def product_checkout_call(product, function, *args, **kwargs):
     """
     Call a function in a product module.
 
-    product -- the name of the product.
+    product -- the product.
     function -- the name of the function.
     args -- the positional arguments to be passed into the function.
     kwargs -- the key-value arguments to be passed into the function.
     """
-    file = os.path.join(
-        data.build.args.products_path, product, CHECKOUT_MODULE)
+    file = os.path.join(PRODUCT_PATH, product.key, CHECKOUT_MODULE)
     if sys.version_info.major >= 3:
         if sys.version_info.minor >= 4:
             import importlib.util
@@ -62,15 +70,17 @@ def get_build_call(product, function):
     """
     Get a function in a product module.
 
-    product -- the name of the product.
+    product -- the product.
     function -- the name of the function.
     """
-    file = os.path.join(data.build.args.products_path, product, BUILD_MODULE)
+    diagnostics.trace("The product path: {}".format(PRODUCT_PATH))
+    diagnostics.trace("The product: {}".format(product))
+    file = os.path.join(PRODUCT_PATH, product.key, BUILD_MODULE)
+    diagnostics.trace(
+        "Loading package {} from file {}".format(BUILD_MODULE, file))
     if sys.version_info.major >= 3:
         if sys.version_info.minor >= 4:
             import importlib.util
-            diagnostics.trace("Loading package {} from file {}".format(
-                BUILD_MODULE, file))
             spec = importlib.util.spec_from_file_location(BUILD_MODULE, file)
             package = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(package)
@@ -89,7 +99,7 @@ def build_call(product, function, *args, **kwargs):
     """
     Call a function in a product module.
 
-    product -- the name of the product.
+    product -- the product.
     function -- the name of the function.
     args -- the positional arguments to be passed into the function.
     kwargs -- the key-value arguments to be passed into the function.
@@ -101,10 +111,10 @@ def build_function_exists(product, function):
     """
     Check whether a function exists in a product module.
 
-    product -- the name of the product.
+    product -- the product.
     function -- the name of the function.
     """
-    file = os.path.join(data.build.args.products_path, product, BUILD_MODULE)
+    file = os.path.join(PRODUCT_PATH, product.key, BUILD_MODULE)
     if sys.version_info.major >= 3:
         if sys.version_info.minor >= 4:
             import importlib.util
@@ -148,18 +158,19 @@ def product_exists(product):
     """
     Check whether a product module exists.
 
-    product -- the name of the product.
+    product -- the product.
     """
-    file = os.path.join(data.build.args.products_path, product)
+    file = os.path.join(PRODUCT_PATH, product.key)
     diagnostics.trace("Looking for module {}".format(file))
     return os.path.isdir(file)
 
 
 def anthem_config_value(variable):
     """Get a configuration value from Unsung Anthem."""
-    name = "config"
-    file = os.path.join(
-        ANTHEM_SOURCE_ROOT, ANTHEM_REPO_NAME, "utils", "config.py")
+    name = "config.py"
+    file = os.path.join(PRODUCT_PATH, "config.py")
+    diagnostics.trace(
+        "Loading package {} from file {}".format(name, file))
     if sys.version_info.major >= 3:
         if sys.version_info.minor >= 4:
             import importlib.util
@@ -175,3 +186,123 @@ def anthem_config_value(variable):
         import imp
         package = imp.load_source(name, file)
         return getattr(package, variable)
+
+
+def get_anthem_common_build_call(function):
+    """
+    Get a function in the common build module in the Unsung Anthem repository.
+
+    function -- the name of the function.
+    """
+    diagnostics.trace("The product path: {}".format(PRODUCT_PATH))
+    diagnostics.trace("The product: {}".format(COMMON_PRODUCT))
+    file = os.path.join(PRODUCT_PATH, COMMON_PRODUCT.key, BUILD_MODULE)
+    diagnostics.trace(
+        "Loading package {} from file {}".format(BUILD_MODULE, file))
+    if sys.version_info.major >= 3:
+        if sys.version_info.minor >= 4:
+            import importlib.util
+            spec = importlib.util.spec_from_file_location(BUILD_MODULE, file)
+            package = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(package)
+            return getattr(package, function)
+        else:
+            from importlib.machinery import SourceFileLoader
+            package = SourceFileLoader(BUILD_MODULE, file).load_module()
+            return getattr(package, function)
+    else:
+        import imp
+        package = imp.load_source(BUILD_MODULE, file)
+        return getattr(package, function)
+
+
+def anthem_common_build_call(function, *args, **kwargs):
+    """
+    Call a function in the common build module in the Unsung Anthem repository.
+
+    function -- the name of the function.
+    args -- the positional arguments to be passed into the function.
+    kwargs -- the key-value arguments to be passed into the function.
+    """
+    return get_anthem_common_build_call(function)(*args, **kwargs)
+
+
+def get_anthem_common_cmake_call(function):
+    """
+    Get a function in the common CMake module in the Unsung Anthem repository.
+
+    function -- the name of the function.
+    """
+    diagnostics.trace("The product path: {}".format(PRODUCT_PATH))
+    diagnostics.trace("The product: {}".format(COMMON_PRODUCT))
+    file = os.path.join(PRODUCT_PATH, COMMON_PRODUCT.key, CMAKE_MODULE)
+    diagnostics.trace(
+        "Loading package {} from file {}".format(CMAKE_MODULE, file))
+    if sys.version_info.major >= 3:
+        if sys.version_info.minor >= 4:
+            import importlib.util
+            spec = importlib.util.spec_from_file_location(CMAKE_MODULE, file)
+            package = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(package)
+            return getattr(package, function)
+        else:
+            from importlib.machinery import SourceFileLoader
+            package = SourceFileLoader(CMAKE_MODULE, file).load_module()
+            return getattr(package, function)
+    else:
+        import imp
+        package = imp.load_source(CMAKE_MODULE, file)
+        return getattr(package, function)
+
+
+def anthem_common_cmake_call(function, *args, **kwargs):
+    """
+    Call a function in the common CMake module in the Unsung Anthem repository.
+
+    function -- the name of the function.
+    args -- the positional arguments to be passed into the function.
+    kwargs -- the key-value arguments to be passed into the function.
+    """
+    return get_anthem_common_cmake_call(function)(*args, **kwargs)
+
+
+def get_anthem_common_directory_call(function):
+    """
+    Get a function in the common directory module in the Unsung Anthem
+    repository.
+
+    function -- the name of the function.
+    """
+    diagnostics.trace("The product path: {}".format(PRODUCT_PATH))
+    diagnostics.trace("The product: {}".format(COMMON_PRODUCT))
+    file = os.path.join(PRODUCT_PATH, COMMON_PRODUCT.key, DIRECTORY_MODULE)
+    diagnostics.trace(
+        "Loading package {} from file {}".format(DIRECTORY_MODULE, file))
+    if sys.version_info.major >= 3:
+        if sys.version_info.minor >= 4:
+            import importlib.util
+            spec = importlib.util.spec_from_file_location(
+                DIRECTORY_MODULE, file)
+            package = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(package)
+            return getattr(package, function)
+        else:
+            from importlib.machinery import SourceFileLoader
+            package = SourceFileLoader(DIRECTORY_MODULE, file).load_module()
+            return getattr(package, function)
+    else:
+        import imp
+        package = imp.load_source(DIRECTORY_MODULE, file)
+        return getattr(package, function)
+
+
+def anthem_common_directory_call(function, *args, **kwargs):
+    """
+    Call a function in the common directory module in the Unsung Anthem
+    repository.
+
+    function -- the name of the function.
+    args -- the positional arguments to be passed into the function.
+    kwargs -- the key-value arguments to be passed into the function.
+    """
+    return get_anthem_common_directory_call(function)(*args, **kwargs)
