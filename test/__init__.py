817d2485173af8e28ab945437fef3557711d4dfb
Antti Kivi <antti.kivi@visiosto.fi>
2019-09-24 02:05:30 +0300

    :white_check_mark: Add the test directory

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------- #
+#                       Couplet Composer
+# ------------------------------------------------------------- #
+#
+# This source file is part of the Couplet Composer project which
+# is part of the Obliging Ode and Unsung Anthem project.
+#
+# Copyright 2019 Antti Kivi
+# Licensed under the EUPL, version 1.2
+#
+# ------------------------------------------------------------- #
