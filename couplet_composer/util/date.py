2e62cef89f00eb422d41f3c08f92bc4a05d3f7f8
Antti Kivi <antti.kivi@visiosto.fi>
2019-09-24 11:03:31 +0300

    :feet: Add date and time utilities

---
+++ couplet_composer/util/date.py
@@ -0,0 +1,71 @@
+# ------------------------------------------------------------- #
+#                       Couplet Composer
+# ------------------------------------------------------------- #
+#
+# This source file is part of the Couplet Composer project which
+# is part of the Obliging Ode and Unsung Anthem project.
+#
+# Copyright 2019 Antti Kivi
+# Licensed under the EUPL, version 1.2
+#
+# ------------------------------------------------------------- #
+
+"""This support module helps to handle date and time objects."""
+
+from datetime import datetime, time
+
+
+def date_difference(first, second):
+    """
+    Calculate the time between two dates and times in seconds.
+
+    first -- the first datetime
+    second -- the second datetime
+    """
+    d = second - first
+    return d.days * 24 * 3600 + d.seconds
+
+
+def to_days(s):
+    """
+    Converts seconds into days, hours, minutes, seconds.
+
+    s -- time in seconds
+    """
+    minutes, seconds = divmod(s, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    return days, hours, minutes, seconds
+
+
+def to_date_string(s):
+    """
+    Converts seconds into a string of days, hours, minutes, and
+    seconds.
+
+    s -- time in seconds
+    """
+    (days, hours, minutes, seconds) = to_days(s)
+
+    if days == 1:
+        days_str = "1 day"
+    else:
+        days_str = "{} days".format(days)
+
+    if hours == 1:
+        hours_str = "1 hour"
+    else:
+        hours_str = "{} hours".format(hours)
+
+    if minutes == 1:
+        minutes_str = "1 minute"
+    else:
+        minutes_str = "{} minutes".format(minutes)
+
+    if seconds == 1:
+        seconds_str = "1 second"
+    else:
+        seconds_str = "{} seconds".format(seconds)
+
+    return "{}, {}, {}, and {}".format(
+        days_str, hours_str, minutes_str, seconds_str)
---
+++ test/util/test_date.py
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------- #
+#                       Couplet Composer
+# ------------------------------------------------------------- #
+#
+# This source file is part of the Couplet Composer project which
+# is part of the Obliging Ode and Unsung Anthem project.
+#
+# Copyright 2019 Antti Kivi
+# Licensed under the EUPL, version 1.2
+#
+# ------------------------------------------------------------- #
+
+"""This module defines the tests for the date utilities."""
+
+from datetime import datetime
+
+from couplet_composer.util import date
+
+
+def test_date_difference():
+    first = datetime.strptime("1990-07-27 00:00:00", "%Y-%m-%d %H:%M:%S")
+    second = datetime.strptime("2011-11-11 00:00:00", "%Y-%m-%d %H:%M:%S")
+    assert date.date_difference(first, second) == 671932800
+
+
+def test_to_days():
+    s = 1432604
+    (days, hours, minutes, seconds) = date.to_days(s)
+    assert days == 16
+    assert hours == 13
+    assert minutes == 56
+    assert seconds == 44
+
+
+class TestToDateString:
+    def test_to_date_string_basic(self):
+        s = 2742513
+        expected = "31 days, 17 hours, 48 minutes, and 33 seconds"
+        assert date.to_date_string(s) == expected
+
+    def test_to_date_string_one_day(self):
+        s = 142513
+        expected = "1 day, 15 hours, 35 minutes, and 13 seconds"
+        assert date.to_date_string(s) == expected
+
+    def test_to_date_string_one_hour(self):
+        s = 259200 + 4352
+        expected = "3 days, 1 hour, 12 minutes, and 32 seconds"
+        assert date.to_date_string(s) == expected
+
+    def test_to_date_string_one_minute(self):
+        s = 259200 + 72082
+        expected = "3 days, 20 hours, 1 minute, and 22 seconds"
+        assert date.to_date_string(s) == expected
+
+    def test_to_date_string_one_second(self):
+        s = 259200 + 61921
+        expected = "3 days, 17 hours, 12 minutes, and 1 second"
+        assert date.to_date_string(s) == expected
