6ad61b37b20a7265ed953e5a032e872b3f885c14
Antti Kivi <antti.kivi@visiosto.fi>
2019-10-10 00:27:02 +0300

    :feet: Add base class for products

---
+++ couplet_composer/products/product.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------- #
+#                       Couplet Composer
+# ------------------------------------------------------------- #
+#
+# This source file is part of the Couplet Composer project which
+# is part of the Obliging Ode and Unsung Anthem project.
+#
+# Copyright (c) 2019 Antti Kivi
+# Licensed under the MIT License
+#
+# ------------------------------------------------------------- #
+
+
+class Product(object):
+    """
+    The base type of types that represent the products that are
+    required or built by the composer.
+    """
