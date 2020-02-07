552fbe5aff7ad3d4322602f5d0e0af3ebc2ea118
Antti Kivi <antti.kivi@visiosto.fi>
2020-02-07 22:26:31 +0200

    Modify the tool data to allow better compiler support and predefined tool paths

--- couplet_composer/support/tool_data.py
+++ couplet_composer/support/tool_data.py
@@ -35,8 +35,12 @@ from ..util.cache import cached
 # get_tool_name -- Returns the name of the tool.
 #
 # get_searched_tool -- Returns a string that represents the name
-# that is used if the tool is looked for from the system. TODO:
-# Parameters
+# that is used if the tool is looked for from the system or a
+# predefined path to the tool executable. TODO: Parameters
+#
+# use_predefined_path -- Returns boolean indicating whether or
+# not the function 'get_searched_tool' contains a predefined path
+# to the tool executable. TODO: Parameters
 #
 # get_required_local_version -- Returns a string that represents
 # version of the tool that is downloaded when the tool isn't
@@ -58,6 +62,7 @@ ToolData = namedtuple("ToolData", [
     "get_tool_key",
     "get_tool_name",
     "get_searched_tool",
+    "use_predefined_path",
     "get_required_local_version",
     "get_local_executable",
     "install_tool"
@@ -77,7 +82,7 @@ def list_tool_types():
     return ["compiler", "cmake", "build_system", "scm", "make"]
 
 
-def _create_tool_data(module_name):
+def _create_tool_data(module_name, tool_name):
     """
     Creates a common ToolData object of a tool for toolchain.
     This function isn't totally pure as it imports the module
@@ -85,6 +90,8 @@ def _create_tool_data(module_name):
 
     module_name -- The name of the module from which the various
     functions are got.
+
+    tool_name -- The name of the tool.
     """
     package_name = "{}.tools.{}".format(
         get_project_package_name(),
@@ -92,29 +99,102 @@ def _create_tool_data(module_name):
     )
     tool_module = importlib.import_module(package_name)
     return ToolData(
-        get_tool_type=getattr(tool_module, "get_tool_type"),
+        get_tool_key=lambda: module_name,
+        get_tool_name=lambda: tool_name,
         get_searched_tool=lambda: module_name,
+        use_predefined_path=lambda: False,
         get_required_local_version=getattr(tool_module, "get_version"),
         get_local_executable=getattr(tool_module, "get_local_executable"),
         install_tool=getattr(tool_module, "install_tool")
     )
 
 
-def create_compiler_tool_data(tool_type, tool_name, version=None):
+def create_unix_compiler_tool_data(
+    cc_name,
+    cxx_name,
+    tool_name,
+    version=None,
+    cc_path=None,
+    cxx_path=None
+):
     """
-    Creates the ToolData object of a generic compiler tool.
+    Creates the ToolData object of a compiler tool on a Unix
+    system.
 
-    tool_type -- The type of the tool.
+    cc_name -- The name of the C compiler.
+
+    cxx_name -- The name of the C++ compiler.
 
     tool_name -- The name of the tool.
 
     version -- The version of compiler tool to search for.
+
+    cc_path -- An optional predefined path to the C compiler.
+
+    cxx_path -- An optional predefined path to the C++ compiler.
+    """
+    def _create_compiler_tool_data(tool_key):
+        return ToolData(
+            get_tool_key=lambda: tool_key,
+            get_tool_name=lambda: tool_name,
+            get_searched_tool=(lambda: tool_key) if not version
+            else (lambda: "{}-{}".format(tool_key, version)),
+            use_predefined_path=lambda: False,
+            get_required_local_version=None,
+            get_local_executable=None,
+            install_tool=None
+        )
+
+    def _create_compiler_tool_data_with_path(tool_key, tool_path):
+        return ToolData(
+            get_tool_key=lambda: tool_key,
+            get_tool_name=lambda: tool_name,
+            get_searched_tool=lambda: tool_path,
+            use_predefined_path=lambda: True,
+            get_required_local_version=None,
+            get_local_executable=None,
+            install_tool=None
+        )
+
+    return CompilerToolPair(
+        cc=_create_compiler_tool_data(tool_key=cc_name) if not cc_path
+        else _create_compiler_tool_data_with_path(
+            tool_key=cc_name,
+            tool_path=cc_path
+        ),
+        cxx=_create_compiler_tool_data(tool_key=cxx_name) if not cxx_path
+        else _create_compiler_tool_data_with_path(
+            tool_key=cxx_name,
+            tool_path=cxx_path
+        )
+    )
+
+
+def create_windows_compiler_tool_data(
+    tool_key,
+    tool_name,
+    version=None,
+    tool_path=None
+):
+    """
+    Creates the ToolData object of a compiler tool on a Windows
+    system.
+
+    tool_key -- The name of the compiler.
+
+    tool_name -- The name of the tool.
+
+    version -- The version of compiler tool to search for. This
+    parameter isn't currently used in the function.
+
+    tool_path -- An optional predefined path to the compiler.
     """
     return ToolData(
-        get_tool_type=lambda: tool_type,
-        get_searched_tool=(lambda: tool_name)
-        if not version
-        else (lambda: "{}-{}".format(tool_name, version)),
+        get_tool_key=lambda: tool_key,
+        get_tool_name=lambda: tool_name,
+        get_searched_tool=(lambda: tool_name) if not tool_path
+        else (lambda: tool_path),
+        use_predefined_path=(lambda: True) if tool_path else (lambda: False),
         get_required_local_version=None,
         get_local_executable=None,
         install_tool=None
@@ -123,74 +203,54 @@ def create_compiler_tool_data(tool_type, tool_name, version=None):
 
 def create_clang_tool_data(version=None):
     """
-    Creates the ToolData object of Clang for toolchain.
+    Creates the ToolData objects of Clang for toolchain. Returns
+    a named tuple of type 'CompilerToolPair'.
 
     version -- The version of Clang to search for.
     """
-    return create_compiler_tool_data(
-        tool_type="cc",
-        tool_name="clang",
-        version=version
-    )
-
-
-def create_clangxx_tool_data(version=None):
-    """
-    Creates the ToolData object of Clang++ for toolchain.
-
-    version -- The version of Clang to search for.
-    """
-    return create_compiler_tool_data(
-        tool_type="cxx",
-        tool_name="clang++",
+    return create_unix_compiler_tool_data(
+        cc_name="clang",
+        cxx_name="clang++",
+        tool_name="Clang",
         version=version
     )
 
 
 def create_gcc_tool_data(version=None):
     """
-    Creates the ToolData object of GCC for toolchain.
+    Creates the ToolData objects of GCC for toolchain. Returns
+    a named tuple of type 'CompilerToolPair'.
 
     version -- The version of GCC to search for.
     """
-    return create_compiler_tool_data(
-        tool_type="cc",
-        tool_name="gcc",
+    return create_unix_compiler_tool_data(
+        cc_name="gcc",
+        cxx_name="g++",
+        tool_name="GCC",
         version=version
     )
 
 
-def create_gxx_tool_data(version=None):
+def create_msvc_tool_data(tool_path=None):
     """
-    Creates the ToolData object of G++ for toolchain.
+    Creates the ToolData object of MSVC.
 
-    version -- The version of GCC to search for.
+    tool_path -- An optional predefined path to the compiler.
     """
-    return create_compiler_tool_data(
-        tool_type="cxx",
-        tool_name="g++",
-        version=version
-    )
-
-
-def create_msvc_tool_data(version=None):
-    """
-    Creates the ToolData object of MSVC compiler.
-
-    version -- The version of GCC to search for.
-    """
-    return create_compiler_tool_data(
-        tool_type="cc",
-        tool_name="cl",
-        version=version
+    return create_windows_compiler_tool_data(
+        tool_key="cl",
+        tool_name="MSVC",
+        tool_path=tool_path
     )
 
 
 def create_make_tool_data():
     """Creates the ToolData object of Make for toolchain."""
     return ToolData(
-        get_tool_type=lambda: "build_system",
+        get_tool_key=lambda: "make",
+        get_tool_name=lambda: "Make",
         get_searched_tool=lambda: "make",
+        use_predefined_path=lambda: False,
         get_required_local_version=None,
         get_local_executable=None,
         install_tool=None
@@ -200,8 +260,10 @@ def create_make_tool_data():
 def create_msbuild_tool_data():
     """Creates the ToolData object of MSBuild for toolchain."""
     return ToolData(
-        get_tool_type=lambda: "build_system",
+        get_tool_key=lambda: "msbuild",
+        get_tool_name=lambda: "MSBuild",
         get_searched_tool=lambda: "msbuild",
+        use_predefined_path=lambda: False,
         get_required_local_version=None,
         get_local_executable=None,
         install_tool=None
@@ -211,8 +273,10 @@ def create_msbuild_tool_data():
 def create_git_tool_data():
     """Creates the ToolData object of Git for toolchain."""
     return ToolData(
-        get_tool_type=lambda: "git",
+        get_tool_key=lambda: "git",
+        get_tool_name=lambda: "Git",
         get_searched_tool=lambda: "git",
+        use_predefined_path=lambda: False,
         get_required_local_version=None,
         get_local_executable=None,
         install_tool=None
@@ -225,7 +289,7 @@ def create_cmake_tool_data():
     function isn't totally pure as it imports the module using
     importlib.
     """
-    return _create_tool_data("cmake")
+    return _create_tool_data(module_name="cmake", tool_name="CMake")
 
 
 def create_ninja_tool_data():
@@ -234,4 +298,4 @@ def create_ninja_tool_data():
     function isn't totally pure as it imports the module using
     importlib.
     """
-    return _create_tool_data("ninja")
+    return _create_tool_data(module_name="ninja", tool_name="Ninja")
