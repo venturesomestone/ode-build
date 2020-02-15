b25f38e368c99deedb2118bc20ba3abada90024f
Antti Kivi <antti.kivi@visiosto.fi>
2020-02-16 00:01:00 +0200

    Add the cases for creating MSVC tool data

--- couplet_composer/modes.py
+++ couplet_composer/modes.py
@@ -50,7 +50,7 @@ from .support.project_names import get_ode_repository_name, get_project_name
 from .support.tool_data import \
     create_clang_tool_data, create_cmake_tool_data, create_gcc_tool_data, \
     create_git_tool_data, create_make_tool_data, create_msbuild_tool_data, \
-    create_ninja_tool_data
+    create_msvc_tool_data, create_ninja_tool_data
 
 from .util.target import parse_target_from_argument_string
 
@@ -267,8 +267,20 @@ def _construct_tool_data(arguments, host_system):
                     return create_gcc_tool_data(
                         version=arguments.compiler_version
                     )
+                else:
+                    return None
         else:
-            pass
+            if arguments.host_compiler:
+                if "msvc" in arguments.host_compiler.lower() \
+                        or "cl.exe" in arguments.host_compiler.lower():
+                    return create_msvc_tool_data(
+                        tool_path=arguments.host_compiler
+                    )
+            else:
+                if arguments.compiler_toolchain == get_msvc_toolchain_name():
+                    return create_msvc_tool_data()
+                else:
+                    return None
 
     def _create_build_system_data(arguments):
         """
