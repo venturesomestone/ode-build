39cc7d2fa5118745a5709a5181b9c2efc40c1f6a
Antti Kivi <antti.kivi@visiosto.fi>
2020-03-18 19:33:21 +0200

    Try appending dash-dash to the end of the preset call

--- couplet_composer/preset_mode.py
+++ couplet_composer/preset_mode.py
@@ -130,7 +130,7 @@ def compose_preset_call(arguments, file_names):
     )
 
     build_call.extend(preset_arguments)
-    # TODO: build_call.append("--")
+    build_call.append("--")
     build_call.extend(preset_arguments_after_end)
 
     return build_call
