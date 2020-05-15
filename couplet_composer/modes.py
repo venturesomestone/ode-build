2d5a1f2b2f098d3105f0b2c42a776056358d521b
Antti Kivi <antti.kivi@visiosto.fi>
2020-05-15 12:40:52 +0300

    Separate the GitHub access helper into its own module

--- couplet_composer/modes.py
+++ couplet_composer/modes.py
@@ -13,6 +13,8 @@ import os
 import sys
 import time
 
+from .github.access import get_api_access_values
+
 from .support.cmake_generators import \
     get_make_cmake_generator_name, get_ninja_cmake_generator_name, \
     get_visual_studio_16_cmake_generator_name
@@ -110,79 +112,6 @@ def run_in_preset_mode(arguments, source_root):
     return 0
 
 
-def _get_github_api_access_values(
-    source_root,
-    value_file,
-    user_agent,
-    api_token
-):
-    """
-    Resolves the user agent and API token to be used to access
-    the version 4 of the GitHub API, if they can be resolved.
-    Returns two values, the first of which is the user agent to
-    be used and the second the API token.
-
-    source_root -- Path to the directory that is the root of the
-    script run.
-
-    value_file -- The pat from arguments to the file from which
-    the user agent and API token can be read.
-
-    user_agent -- The user agent given through command line
-    arguments that is used when accessing the GitHub API.
-
-    api_token -- The GitHub API token given through command line
-    arguments that is used to access the API.
-    """
-    repository_root = os.path.join(source_root, get_ode_repository_name())
-    value_file_path = os.path.join(repository_root, value_file)
-    api_file_content = []
-
-    if value_file and os.path.exists(value_file_path):
-        logging.debug(
-            "Found a file containing the user agent and authorization token "
-            "for GitHub API from %s",
-            value_file_path
-        )
-        with open(value_file_path) as api_file:
-            api_file_content = [line.strip() for line in api_file.readlines()]
-    elif not os.path.exists(value_file_path):
-        logging.debug(
-            "The file containing the user agent and authorization token for "
-            "GitHub API wasn't found from %s",
-            value_file_path
-        )
-
-    return_user_agent = None
-    return_api_token = None
-
-    if api_file_content:
-        logging.debug("Adding the user agent and the API token from the file")
-        return_user_agent = api_file_content[0]
-        return_api_token = api_file_content[1]
-
-    if user_agent:
-        logging.debug("Adding the user agent from command line")
-        return_user_agent = user_agent
-
-    if api_token:
-        logging.debug("Adding the API token from command line")
-        return_api_token = api_token
-
-    if return_user_agent and return_api_token:
-        logging.info(
-            "Using the GraphQL API of GitHub as user agent and access token "
-            "are given"
-        )
-    else:
-        logging.info(
-            "Using the REST API of GitHub as user agent and access token "
-            "aren't given"
-        )
-
-    return return_user_agent, return_api_token
-
-
 def run_in_configuring_mode(arguments, source_root):
     """
     Runs the script in configuration mode and sets up the
@@ -211,7 +140,7 @@ def run_in_configuring_mode(arguments, source_root):
 
     logging.debug("Creating the toolchain for the run")
 
-    github_user_agent, github_api_token = _get_github_api_access_values(
+    github_user_agent, github_api_token = get_api_access_values(
         source_root=source_root,
         value_file=arguments.github_auth_file,
         user_agent=arguments.github_user_agent,
@@ -318,7 +247,7 @@ def run_in_composing_mode(arguments, source_root):
 
     logging.debug("Creating the toolchain for the run")
 
-    github_user_agent, github_api_token = _get_github_api_access_values(
+    github_user_agent, github_api_token = get_api_access_values(
         source_root=source_root,
         value_file=arguments.github_auth_file,
         user_agent=arguments.github_user_agent,
