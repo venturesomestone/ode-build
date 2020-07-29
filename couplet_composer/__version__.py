acd3d32274b9ed95a9a6f490f00d2b0cbcd36326
Antti Kivi <antti.kivi@visiosto.fi>
2020-07-29 21:28:41 +0300

    Update version number

--- couplet_composer/__version__.py
+++ couplet_composer/__version__.py
@@ -13,7 +13,7 @@ def get_release_version():
     and, thus, returns three values: the major version number,
     the minor version number, and the patch version number.
     """
-    return 1, 1, 3
+    return 1, 1, 4
 
 
 def get_version():
