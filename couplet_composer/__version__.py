246124c5e2e2da0978f5539ced0f3c0f02ddb253
Antti Kivi <antti.kivi@visiosto.fi>
2021-02-27 18:33:44 +0200

    Bump version to v1.7.2

--- couplet_composer/__version__.py
+++ couplet_composer/__version__.py
@@ -15,7 +15,7 @@ def get_release_version():
     and, thus, returns three values: the major version number,
     the minor version number, and the patch version number.
     """
-    return 1, 7, 1
+    return 1, 7, 2
 
 
 _VERSION_SUFFIX = ""
